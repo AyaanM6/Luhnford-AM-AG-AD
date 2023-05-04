126a6d2f4f4778c9dfc8b4739a7f2549d74415bd
AyaanM6 <Ayaanm2006@gmail.com>
2023-05-04 14:14:30 -0400

    Created Skeleton

---
+++ CustomerSystem.java
@@ -0,0 +1,38 @@
+public class CustomerSystem{
+    public static void printMenu(){
+        // print menu
+    }
+
+    public static void enterCustomerInfo(){
+        // get user input firstname lastname city
+    }
+
+    public static void validatePostalCode(){
+
+    }
+
+    public static void reverseCard(){
+
+    }
+    
+    public static void evenSum(){
+
+    }
+
+    public static void oddSum(){
+
+    }
+
+    public static void validateCreditCard(){
+
+    }
+
+    public static void 
+
+    public static void main(String[] args) {
+    
+
+
+    }
+}
+    
