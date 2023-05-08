885b745b3dc7e2ad7792fd38b9c568f3bc83305f
annika daspal <annika.daspal@gmail.com>
2023-05-08 14:04:54 -0400

    Edited main and finished skeleton

--- CustomerSystem.java
+++ CustomerSystem.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+import java.util.ArrayList;
 public class CustomerSystem{
     public static void printMenu(){
         // print menu
@@ -73,9 +75,46 @@ public class CustomerSystem{
     }
 
     public static void main(String[] args) {
-    
+        Scanner reader = new Scanner(System.in);
+        String userInput = "";
+        String enterCustomerOption = "1";
+        String generateCustomerOption = "2";
+        String reportSalesOption = "3";
+        String checkFraudOption = "4";
+        String exitCondition = "9";
+        int number = 0;
 
+        // More variables for the main may be declared in the space below
 
+        while (!userInput.equals(exitCondition)){
+            printMenu();
+            System.out.println("Enter a number: ");          
+            userInput = reader.nextLine();       // User selection from the menu                    // Printing out the main menu
+
+            if (userInput.equals(enterCustomerOption)){
+                // Only the line below may be editted based on the parameter list and how you design the method return
+                // Any necessary variables may be added to this if section, but nowhere else in the code
+                enterCustomerInfo();
+            }
+            else if (userInput.equals(generateCustomerOption)){
+                // Only the line below may be editted based on the parameter list and how you design the method return
+                generateCustomerDataFile();
+            }
+            else if (userInput.equals(reportSalesOption)){
+                String filePath = validateSalesFile();
+                ArrayList<String> salesList = new ArrayList<String>();
+                salesList = createSalesList();
+            }
+            else if (userInput.equals(checkFraudOption)){ 
+                checkFraud();
+            }
+            else{
+                System.out.println("Please type in a valid option (A number from 1-9)");
+                userInput = reader.nextLine(); 
+            }
+
+        // Exits once the user types 
+        System.out.println("Program Terminated");
+        }
     }
-}
-    
+} 
